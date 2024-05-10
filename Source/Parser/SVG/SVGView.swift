70550e3db7ea85ffb6ef7edb7f46fda331c63926
Kevin van den Hoek <kevinvandenhoek@gmail.com>
2024-05-10 23:34:40 +0200

    fix main thread issue

--- Source/Parser/SVG/SVGView.swift
+++ Source/Parser/SVG/SVGView.swift
@@ -9,34 +9,40 @@ import SwiftUI
 
 public struct SVGView: View {
 
-    public let svg: SVGNode?
+    private let url: URL?
+    @State private var svg: SVGNode?
 
     public init(contentsOf url: URL) {
-        self.svg = SVGParser.parse(contentsOf: url)
+        self.url = url
     }
 
     @available(*, deprecated, message: "Use (contentsOf:) initializer instead")
     public init(fileURL: URL) {
-        self.svg = SVGParser.parse(contentsOf: fileURL)
+        self.url = fileURL
     }
 
     public init(data: Data) {
+        self.url = nil
         self.svg = SVGParser.parse(data: data)
     }
 
     public init(string: String) {
+        self.url = nil
         self.svg = SVGParser.parse(string: string)
     }
 
     public init(stream: InputStream) {
+        self.url = nil
         self.svg = SVGParser.parse(stream: stream)
     }
 
     public init(xml: XMLElement) {
+        self.url = nil
         self.svg = SVGParser.parse(xml: xml)
     }
 
     public init(svg: SVGNode) {
+        self.url = nil
         self.svg = svg
     }
 
@@ -45,7 +51,24 @@ public struct SVGView: View {
     }
 
     public var body: some View {
-        svg?.toSwiftUI()
+        if let svg {
+            svg.toSwiftUI()
+        } else if let url {
+            Color.clear
+                .onAppear {
+                    Task { await load(url: url) }
+                }
+        }
     }
 
+}
+
+extension SVGView {
+    
+    func load(url: URL) async {
+        let svg = SVGParser.parse(contentsOf: url)
+        await MainActor.run {
+            self.svg = svg
+        }
+    }
 }
